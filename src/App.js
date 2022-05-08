fc26a3aeb4c9ae6cabd6bc31a9a57769c552ee03
JonaPortillo <portillo.jonathan.leonel@gmail.com>
2022-05-07 21:03:49 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './components/Botón';
+import Contador from './components/Contador';
+import logoFCC from './images/logo.jpg'
 
 function App() {
+
+  const manejarClick = () => {
+    console.log("Click")
+  }
+
+  const reiniciarContador = () => {
+    console.log("Reiniciar")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          className="freecodecamp-logo"
+          src={logoFCC}
+          alt="Logo freeCodeCamp" />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador
+          numClicks='5'
+        />
+        <Boton
+          texto='Click'
+          esBotonDeClick={true}
+          manejarClick={manejarClick} />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador} />
+      </div>
     </div>
   );
 }
